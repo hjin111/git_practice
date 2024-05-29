6f6723622d479f220170118f712493cc477f6f46
hjin125 <https://github.com/hjin111>
2024-05-29 16:40:25 +0900

    fetch

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -41,8 +41,13 @@ git checkout master
 git pull origin 브랜치명
 
 # git 작업 취소 
-# working directory에서의 취소
+# working directory에서의 수정사항만 취소
+# 수정사항만 취소
 git checkout .
+# working directory에서 추가파일 취소
+git clean --fdx
+# 수정취소 동시에
+git checkout . | git clean --fdx
 # staging area에서의 취소
 git reset
 # commit 이후의 취소
@@ -51,4 +56,8 @@ git reset HEAD^
 # push 이후의 취소
 git revert 커밋ID
 
-
+# 버전별 비교 명령어 : diff
+# A를 기준으로 B가 어떤 변경이 있는지를 비교 ( 커밋 id는 적당히 복사하면 됨 )
+git diff A B 
+git diff commitId1 commitId2
+git diff A브랜치 B브랜치
