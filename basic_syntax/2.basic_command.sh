c558347d62fba785054f3f0ae8b79c28b511e141
hjin125 <https://github.com/hjin111>
2024-05-29 15:02:33 +0900

    vi commit this is vi commit`

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -1,6 +1,16 @@
 # 현재 working directory, staging area 만 상태 확인
 git status
 
+# git add : 모든 수정/추가/사항 add
+git add . 
+
+# git add 특정파일 : 특정 파일 add
+git add 특정파일(위치)
+
+# local repository에 반영 및 커밋이력 생성
+git commit -m "메시지타이틀" -m "메시지내용"
+
 # commit 이력 확인(local repository의 이력)
 git log
+
 
